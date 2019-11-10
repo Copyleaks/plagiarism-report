dd813e2692c809f4536e9fe87e62a7bd0f666b83
tal-copyleaks <tala@copyleaks.com>
2019-11-10 11:33:29 +0200

    fix outdated polifills file

--- client/projects/demo-app/src/polyfills.ts
+++ client/projects/demo-app/src/polyfills.ts
@@ -17,7 +17,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-import 'core-js/es7';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
