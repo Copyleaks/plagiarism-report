4c53215303c77ed270a1666a80cde83912d355df
tal-copyleaks <tala@copyleaks.com>
2020-01-21 10:12:43 +0200

    add missing exports to public api

--- client/projects/plagiarism-report/src/lib/plagiarism-report/models/index.ts
+++ client/projects/plagiarism-report/src/lib/plagiarism-report/models/index.ts
@@ -3,6 +3,7 @@ export * from './api-models/NewResult';
 export * from './api-models/ScanSource';
 export * from './api-models/ScanResult';
 export * from './CopyleaksReportConfig';
+export * from './CopyleaksTextConfig';
 export * from './ResultsSettings';
 export * from './Matches';
 export * from './HtmlHelper';
--- client/projects/plagiarism-report/src/public-api.ts
+++ client/projects/plagiarism-report/src/public-api.ts
@@ -10,7 +10,14 @@ export * from './lib/plagiarism-report/copyleaks-report.component';
 export * from './lib/plagiarism-report/copyleaks-report.module';
 // models
 export * from './lib/plagiarism-report/models';
-
+// validation helpers
 export * from './lib/plagiarism-report/utils/validation';
 
-export { DEFAULT_REPORT_CONFIG, DEFAULT_OPTIONS } from './lib/plagiarism-report/utils/constants';
+// pre-defined configurations
+export { DEFAULT_REPORT_CONFIG, DEFAULT_OPTIONS, DEFAULT_TEXT_CONFIG } from './lib/plagiarism-report/utils/constants';
+
+// injection tokens
+export {
+	COPYLEAKS_CONFIG_INJECTION_TOKEN,
+	COPYLEAKS_TEXT_CONFIG_INJECTION_TOKEN,
+} from './lib/plagiarism-report/utils/constants';
