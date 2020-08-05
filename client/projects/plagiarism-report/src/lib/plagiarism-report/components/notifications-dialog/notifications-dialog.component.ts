69aa4049fc3cd7490b32a15f4245251580f42058
Bayan Abuawad <bayana@copyleaks.com>
2020-08-05 09:58:06 +0300

    finished working on notifications support

--- client/projects/plagiarism-report/src/lib/plagiarism-report/components/notifications-dialog/notifications-dialog.component.ts
+++ client/projects/plagiarism-report/src/lib/plagiarism-report/components/notifications-dialog/notifications-dialog.component.ts
@@ -1,55 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { CompleteResultNotification, CompleteResultNotificationAlertSeverity } from '../../models';
-import { MatDialogRef } from '@angular/material/dialog';
+import { ReportService } from '../../services/report.service';
+import { untilDestroy } from '../../../shared/operators/untilDestroy';
+import { filter } from 'rxjs/operators';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
 	selector: 'cr-notifications-dialog',
 	templateUrl: './notifications-dialog.component.html',
 	styleUrls: ['./notifications-dialog.component.scss']
 })
-export class NotificationsDialogComponent implements OnInit {
+export class NotificationsDialogComponent implements OnInit, OnDestroy {
 	notification: CompleteResultNotification;
 	severity = CompleteResultNotificationAlertSeverity;
-	constructor(dialogRef: MatDialogRef<NotificationsDialogComponent>) {
-		dialogRef.addPanelClass('copyleaks-report-dialog');
-	}
+	constructor(@Inject(MAT_DIALOG_DATA) private reportService: ReportService) { }
+	/**
+ 	* Life-cycle method
+ 	*/
 	ngOnInit() {
-		this.notification = {
-			alerts: [
-				{
-					code: 'repository-scan-failed',
-					title: 'Shiba Inu',
-					message: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-					helpLink: null,
-					severity: 1,
-					additionalData: '{"repositoryId":"a","repositoryName":"a"}'
-				},
-				{
-					code: 'repository-index-failed',
-					title: 'Not able to Index Against Repository',
-					message: 'You do not have permission to index against `a` or the repository does not exists.',
-					helpLink: null,
-					severity: 2,
-					additionalData: '{"repositoryId":"a","repositoryName":"a"}'
-				},
-				{
-					code: 'repository-index-failed',
-					title: 'Not able to Index Against Repository',
-					message: 'You do not have permission to index against `a` or the repository does not exists.',
-					helpLink: null,
-					severity: 3,
-					additionalData: '{"repositoryId":"a","repositoryName":"a"}'
-				},
-				{
-					code: 'repository-index-failed',
-					title: 'Not able to Index Against Repository',
-					message: 'You do not have permission to index against `a` or the repository does not exists.',
-					helpLink: null,
-					severity: 4,
-					additionalData: '{"repositoryId":"a","repositoryName":"a"}'
-				}
-			]
-		}
+		this.reportService.completeResult$
+			.pipe(
+				untilDestroy(this),
+				filter(c => !!c.notifications && !!c.notifications.alerts && !!c.notifications.alerts.length)
+			)
+			.subscribe(completeResult => {
+				this.notification = completeResult.notifications;
+			});
 	}
-
+	/**
+	 * Life-cycle method
+	 * empty for `untilDestroy` rxjs operator
+	 */
+	ngOnDestroy() { }
 }
--- client/projects/plagiarism-report/src/lib/plagiarism-report/components/notifications/notifications.component.ts
+++ client/projects/plagiarism-report/src/lib/plagiarism-report/components/notifications/notifications.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationsDialogComponent } from '../notifications-dialog/notifications-dialog.component';
-enum NotificationType {
-	Error,
-	Warning,
-	Info
-}
+import { ReportService } from '../../services/report.service';
+import { untilDestroy } from '../../../shared/operators/untilDestroy';
+import { filter } from 'rxjs/operators';
+import { CompleteResultNotificationAlertSeverity } from '../../models';
 @Component({
 	selector: 'cr-notifications',
 	templateUrl: './notifications.component.html',
 	styleUrls: ['./notifications.component.scss']
 })
-export class NotificationsComponent implements OnInit {
-	public type = NotificationType.Error;
-	notificationType = NotificationType;
-	constructor(private matDialog: MatDialog) { }
+export class NotificationsComponent implements OnInit, OnDestroy {
+	public severity: CompleteResultNotificationAlertSeverity;
+	public severities = CompleteResultNotificationAlertSeverity;
+	constructor(private matDialog: MatDialog, private reportService: ReportService) { }
+	/**
+ 	* Life-cycle method
+ 	*/
 	ngOnInit() {
-		this.showNotificationsDialog();
+		this.reportService.completeResult$
+			.pipe(
+				untilDestroy(this),
+				filter(c => !!c.notifications && !!c.notifications.alerts && !!c.notifications.alerts.length)
+			)
+			.subscribe(completeResult => {
+				const alertSeverities = completeResult.notifications.alerts.map(s => +s.severity);
+				this.severity = Math.max(...alertSeverities);
+			});
 	}
+	/**
+ 	* shows the notification dialogs with the alerts
+ 	*/
 	showNotificationsDialog() {
 		this.matDialog.open(NotificationsDialogComponent, {
-			autoFocus: false
+			autoFocus: false,
+			maxWidth: '500px',
+			data: this.reportService
 		})
 	}
+	/**
+	 * Life-cycle method
+	 * empty for `untilDestroy` rxjs operator
+	 */
+	ngOnDestroy() { }
 }
