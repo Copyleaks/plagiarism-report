7b8ab4ed2cbe98262360b365e47ebda86e9da6b2
Bayan Abuawad <bayana@copyleaks.com>
2021-04-21 16:41:27 +0300

    bump version

--- client/projects/plagiarism-report/src/lib/plagiarism-report/components/suspect/suspect.component.ts
+++ client/projects/plagiarism-report/src/lib/plagiarism-report/components/suspect/suspect.component.ts
@@ -28,7 +28,7 @@ export class SuspectComponent implements OnInit, OnDestroy {
 		private layoutService: LayoutMediaQueryService,
 		private matchService: MatchService,
 		private translatesService: CopyleaksTranslateService
-	) { }
+	) {}
 	readonly MatchType = MatchType;
 	public isMobile = false;
 	public zoom = 1;
@@ -117,8 +117,8 @@ export class SuspectComponent implements OnInit, OnDestroy {
 			.pipe(untilDestroy(this))
 			.subscribe(
 				([mode, suspect]) =>
-				(this.contentMode =
-					mode === 'html' && suspect && suspect.result && suspect.result.html.value ? 'html' : 'text')
+					(this.contentMode =
+						mode === 'html' && suspect && suspect.result && suspect.result.html.value ? 'html' : 'text')
 			);
 
 		onlyOneToOne$.pipe(untilDestroy(this)).subscribe(disable => (this.disableBackButton = disable));
@@ -138,5 +138,5 @@ export class SuspectComponent implements OnInit, OnDestroy {
 	 * life-cycle method
 	 * empty for `untilDestroy` rxjs operator
 	 */
-	ngOnDestroy() { }
+	ngOnDestroy() {}
 }
