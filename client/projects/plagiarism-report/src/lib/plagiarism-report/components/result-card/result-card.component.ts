e75ab19926ada37d3f8edc50189bff82e79901c1
Bayan Abuawad <bayana@copyleaks.com>
2022-04-14 10:38:34 +0300

    minor fix

--- client/projects/plagiarism-report/src/lib/plagiarism-report/components/result-card/result-card.component.ts
+++ client/projects/plagiarism-report/src/lib/plagiarism-report/components/result-card/result-card.component.ts
@@ -171,7 +171,13 @@ export class ResultCardComponent implements OnInit, OnDestroy {
 		}
 
 		if (this.preview?.metadata?.filename) {
-			this.preview.metadata.filename = decodeURIComponent(this.preview?.metadata?.filename);
+			let filename = this.preview?.metadata?.filename;
+			try {
+				filename = decodeURIComponent(this.preview?.metadata?.filename);
+			} catch {
+				filename = this.preview?.metadata?.filename;
+			}
+			this.preview.metadata.filename = filename;
 		}
 
 		this.previewDate = this.getPreviewDate();
